83e7943e78e043bb6b3b1632bdec886c4c178a76
Mu-Xiang <0muxiang0@gmail.com>
2021-05-04 17:34:08 +0800

    new paddle's paintThread

--- src/BrickGame.java
+++ src/BrickGame.java
@@ -2,6 +2,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Graphics;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -9,9 +10,15 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JDesktopPane;
 import javax.swing.JLabel;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class BrickGame extends JFrame {
 	public static final String proName = "BrickGame";
+	public boolean gameStart;
+	
+	Paddle paddle = new Paddle();
+	ArrayList<Brick> bricks = new ArrayList<Brick>();
 
 	private JPanel contentPane;
 
@@ -37,6 +44,10 @@ public class BrickGame extends JFrame {
 	 * Create the frame.
 	 */
 	public BrickGame() {
+		//initialize
+		gameStart = false;
+		
+		//frame
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 720, 480);
 		contentPane = new JPanel();
@@ -46,6 +57,7 @@ public class BrickGame extends JFrame {
 		
 		JDesktopPane paneGame = new JDesktopPane();
 		contentPane.add(paneGame, BorderLayout.CENTER);
+		paneGame.setBackground(Color.GRAY);
 		
 		JPanel panelInformation = new JPanel();
 		panelInformation.setBounds(535, 10, 150, 24);
@@ -64,13 +76,59 @@ public class BrickGame extends JFrame {
 		panelInformation.add(labelTime);
 		
 		JButton btnStart = new JButton("Start");
+		btnStart.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				gameStart = true;
+				Graphics g = getGraphics();
+				InitDraw(g);
+			}
+		});
 		btnStart.setBounds(160, 340, 80, 30);
 		paneGame.add(btnStart);
 		
 		JButton btnStop = new JButton("Stop");
+		btnStop.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				gameStart = false;
+				Graphics g = getGraphics();
+				g.setColor(Color.GRAY);
+				g.fillRect(paddle.getX() , paddle.getY(), paddle.getWidth() + 1, paddle.getHeight() + 1);
+			}
+		});
 		btnStop.setBounds(480, 340, 80, 30);
 		paneGame.add(btnStop);
 		
-		
+		new Thread(new PaintThread()).start();
+	}
+	
+	public void InitDraw(Graphics g) {
+		paddle = new Paddle(330, 390, 90, 10, Color.WHITE);
+		paddle.draw(g);
+//		bricks.add(new Brick(100, 50, 40, 10, Color.YELLOW));
+//		bricks.get(0).draw(g);
+	}
+	
+	public void Draw(Graphics g) {
+		paddle.draw(g);
+	}
+	
+	private class PaintThread implements Runnable {
+		public void run() {
+			while (true) {
+				try {
+					Thread.sleep(50);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				if (gameStart) {
+					Graphics g = getGraphics();
+					g.setColor(Color.GRAY);
+					g.fillRect(paddle.getX() , paddle.getY(), paddle.getWidth() + 1, paddle.getHeight() + 1);
+					//paddle.setX(paddle.getX() + 5);
+					Draw(g);
+					System.out.println(paddle.getX());
+				}
+			}
+		}
 	}
 }
