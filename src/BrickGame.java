8f1da94f4af6c11995d7226ea7dce718cd38cce2
Mu-Xiang <0muxiang0@gmail.com>
2021-05-05 00:59:58 +0800

    new gamePause

--- src/BrickGame.java
+++ src/BrickGame.java
@@ -14,18 +14,25 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.ActionEvent;
+import java.awt.Font;
 
 public class BrickGame extends JFrame implements KeyListener {
 	public static final String proName = "BrickGame";
 	public boolean gameStart;
+	public boolean gamePause;
+	
+	public int score;
 	public int timerMin;
 	public int timerSec;
-	public String timerText = "00 : 00";
+	public String timerText = "00 : 00 ";
 	
 	Paddle paddle = new Paddle();
+	Ball ball = new Ball();
 	ArrayList<Brick> bricks = new ArrayList<Brick>();
 
+	public JLabel labelScore;
 	public JLabel labelTime;
+	public JLabel labelTip;
 	private JPanel contentPane;
 
 	/**
@@ -52,6 +59,8 @@ public class BrickGame extends JFrame implements KeyListener {
 	public BrickGame() {
 		//initialize
 		gameStart = false;
+		gamePause = false;
+		score = 0;
 		timerMin = 0;
 		timerSec = 0;
 		
@@ -69,68 +78,84 @@ public class BrickGame extends JFrame implements KeyListener {
 		paneGame.setBackground(Color.LIGHT_GRAY);
 		
 		JPanel panelInformation = new JPanel();
-		panelInformation.setBounds(535, 400, 150, 24);
+		panelInformation.setBounds(530, 400, 155, 24);
 		paneGame.add(panelInformation);
 		
-		JLabel labelScoreText = new JLabel("Score : ");
+		JLabel labelScoreText = new JLabel(" Score : ");
 		panelInformation.add(labelScoreText);
 		
-		JLabel labelScore = new JLabel("0");
+		labelScore = new JLabel(score + " ");
 		panelInformation.add(labelScore);
 		
-		JLabel labelTimeText = new JLabel("    Time");
+		JLabel labelTimeText = new JLabel("  Time");
 		panelInformation.add(labelTimeText);
 		
 		labelTime = new JLabel(timerText);
 		panelInformation.add(labelTime);
 		
-		JButton btnStart = new JButton("Start");
-		btnStart.setBounds(160, 340, 80, 30);
-		paneGame.add(btnStart);
-		btnStart.setVisible(false);
+		JDesktopPane paneTip = new JDesktopPane();
+		paneTip.setBackground(Color.LIGHT_GRAY);
+		paneTip.setBounds(10, 400, 400, 24);
+		paneGame.add(paneTip);
 		
-		JButton btnStop = new JButton("Stop");
-		btnStop.setBounds(480, 340, 80, 30);
-		paneGame.add(btnStop);
-		btnStop.setVisible(false);
+		JLabel labelTipText = new JLabel("Tip : ");
+		labelTipText.setFont(new Font("Times New Roman", Font.BOLD, 13));
+		labelTipText.setForeground(Color.BLUE);
+		labelTipText.setBounds(10, 5, 30, 16);
+		paneTip.add(labelTipText);
 		
-		btnStart.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				startGame();
-				System.out.println("Game start.");
-			}
-		});
-		
-		btnStop.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				stopGame();
-			}
-		});
+		labelTip = new JLabel("Press space to new a game.");
+		labelTip.setForeground(Color.BLUE);
+		labelTip.setFont(new Font("Times New Roman", Font.BOLD, 13));
+		labelTip.setBounds(40, 5, 350, 16);
+		paneTip.add(labelTip);
 		
 		this.addKeyListener(this);
 		new Thread(new PaintThread()).start();
 		new Thread(new TimerThread()).start();
 	}
 	
-	public void startGame() {
+	public void newGame() {
 		gameStart = true;
+		gamePause = true;
 		
 		Graphics g = getGraphics();
 		initDraw(g);
+		labelTip.setText("Press space to start the game.");
+	}
+	
+	public void startGame() {
+		gamePause = false;
+		
+		Graphics g = getGraphics();
+		
 	}
 	
 	public void stopGame() {
-		gameStart = false;
+		gamePause = true;
 		
 		Graphics g = getGraphics();
-		g.setColor(Color.LIGHT_GRAY);
-		g.fillRect(paddle.getX() , paddle.getY(), paddle.getWidth() + 1, paddle.getHeight() + 1);
+		
+	}
+	
+	public String tipSelect() {
+		if (gameStart) {
+			if (gamePause)
+				return "Press space to start the game or press N to new a game.";
+			else
+				return "Press space to pause the game.";
+		} else
+			return "Press space to new a game.";
 	}
 	
 	public void initDraw(Graphics g) {
 		//Paddle
-		paddle = new Paddle(330, 390, 90, 10, Color.WHITE);
+		paddle = new Paddle(300, 390, 90, 10, Color.WHITE);
 		paddle.draw(g);
+		
+		//Ball
+		ball = new Ball(340, 380, 10, 10, Color.CYAN);
+		ball.draw(g);
 		
 		//Bricks
 	}
@@ -139,7 +164,7 @@ public class BrickGame extends JFrame implements KeyListener {
 		public void run() {
 			while (true) {
 				try {
-					Thread.sleep(50);
+					Thread.sleep(40);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
@@ -150,6 +175,8 @@ public class BrickGame extends JFrame implements KeyListener {
 					g.fillRect(12, 36, 696, 386);
 					//Paddle
 					paddle.draw(g);
+					//Ball
+					ball.draw(g);
 				}
 			}
 		}
@@ -168,7 +195,7 @@ public class BrickGame extends JFrame implements KeyListener {
 						timerSec = 0;
 						timerMin++;
 					}
-					timerText = ((timerMin < 10) ? "0" : "") + timerMin + " : " + ((timerSec < 10) ? "0" : "") + timerSec;
+					timerText = ((timerMin < 10) ? "0" : "") + timerMin + " : " + ((timerSec < 10) ? "0" : "") + timerSec + " ";
 					labelTime.setText(timerText);
 					//System.out.println(timerText);
 				}
@@ -184,14 +211,20 @@ public class BrickGame extends JFrame implements KeyListener {
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		//System.out.println(e.getKeyCode());
+		System.out.println(e.getKeyCode());
 		switch (e.getKeyCode()) {
 			case KeyEvent.VK_SPACE:
-				if (gameStart) stopGame();
-				else startGame();
+				if (gameStart) {
+					if (gamePause) startGame();
+					else stopGame();
+				} else newGame();
+				labelTip.setText(tipSelect());
+				break;
+			case KeyEvent.VK_N:
+				
 				break;
 			default:
-				paddle.processKeyPressed(e);
+				if (!gamePause) paddle.processKeyPressed(e);
 				break;
 		}
 	}
