3fdd9bb58dc8beab967257b1bb4b5384f4b3d879
Mu-Xiang <0muxiang0@gmail.com>
2021-05-04 16:31:28 +0800

    update gui design

--- src/BrickGame.java
+++ src/BrickGame.java
@@ -6,6 +6,9 @@ import java.awt.Graphics;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JDesktopPane;
+import javax.swing.JLabel;
+import javax.swing.JButton;
 
 public class BrickGame extends JFrame {
 	public static final String proName = "BrickGame";
@@ -22,7 +25,7 @@ public class BrickGame extends JFrame {
 					BrickGame frame = new BrickGame();
 					frame.setVisible(true);
 					frame.setTitle(proName);
-					//frame.setSize(1080, 720);
+					frame.setSize(720, 480);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -35,11 +38,38 @@ public class BrickGame extends JFrame {
 	 */
 	public BrickGame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 720, 480);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JDesktopPane paneGame = new JDesktopPane();
+		contentPane.add(paneGame, BorderLayout.CENTER);
+		
+		JPanel panelInformation = new JPanel();
+		panelInformation.setBounds(535, 10, 150, 24);
+		paneGame.add(panelInformation);
+		
+		JLabel labelScoreText = new JLabel("Score : ");
+		panelInformation.add(labelScoreText);
+		
+		JLabel labelScore = new JLabel("0");
+		panelInformation.add(labelScore);
+		
+		JLabel labelTimeText = new JLabel("    Time");
+		panelInformation.add(labelTimeText);
+		
+		JLabel labelTime = new JLabel("00 : 00");
+		panelInformation.add(labelTime);
+		
+		JButton btnStart = new JButton("Start");
+		btnStart.setBounds(160, 340, 80, 30);
+		paneGame.add(btnStart);
+		
+		JButton btnStop = new JButton("Stop");
+		btnStop.setBounds(480, 340, 80, 30);
+		paneGame.add(btnStop);
 		
 		
 	}
